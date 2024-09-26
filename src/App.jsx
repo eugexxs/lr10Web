31df6b2c292cca7e85bc867fd93048e97bc6ac21
eugexxs <evgenijgurin811@gmail.com>
2024-09-26 14:03:55 +0300

    finished header and started main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import './desktop-style.css'
+import './css/fonts-and-vars.css'
+import './css/header-desktop-style.css'
+import './css/main-desktop-styles.css'
 
 function App() {
   return (
@@ -16,9 +18,26 @@ function App() {
             <span className="header__upper-part__right__element">Contact</span>
           </div>
         </div>
+        <div className="header__middle-part">
+          <div className="header__middle-part__info">
+              <h4>WELCOME</h4>
+              <h1>Lorem ipsum dolor sit amet, consectetur </h1>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam nobis cum est eos error ipsum, voluptate culpa nesciunt delectus iste?</p>
+              <button className = "header__middle-part__button">Explore</button>
+          </div>
+          <picture className = "header__middle-part__image">
+              <img src = "src/imgs/humans.svg"/>
+            </picture>
+        </div>
       </header>
+      <main className = "main">
+        <div className = "main__learn-more">
+          <h4>PARTNERS</h4>
+        </div>
+      </main>
     </>
   )
 }
 
 export default App
+// TODO: Сделать часть main и отцентрировать main__learn-more
